5132974b0a126c97e8f85a84a13b4db6edb763c9
MestreWil <williamtavaresdemoura@gmail.com>
2026-03-02 02:27:46 -0300

    Acessando o valor de um endereco de memoria de maneiras diferentes

---
+++ aula7/ponteiro-de-ponteiros-02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    float z =2.5;
+
+    float *fp;
+
+    fp = &z;
+
+    printf("*&z = %f\n", *&z);
+    printf("*fp = %f\n", *fp);
+    printf("**&fp = %f\n", **&fp);
+
+    return 0;
+}
